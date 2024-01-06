ee3d5f6606eaf803374ea9797ebe2e8bd9a86f61
Loganayaki0212 <155843410+Loganayaki0212@users.noreply.github.com>
2024-01-06 23:55:15 +0530

    Add files via upload
    week1.day1

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args)
+	{
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVariable = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion:" +browserVersion);
+		System.out.println("BrowserName:" +browserName);
+		System.out.println("isVariable is true or false:" +isVariable);
+		System.out.println("releaseYear:" +releaseYear);
+		System.out.println("browserLogo:" +browserLogo);
+	}
+
+}
